2755bccac6ced2d10f6e1e6e60d577caac9bb988
inoublii <inoublii@gmail.com>
2020-03-13 16:36:34 +0100

    compiling errors

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,55 @@
 #include "binary_trees.h"
 #include <stdlib.h>
-#include <math.h>
+
+int _pow_(int x, int y)
+{
+	if (y > 0)
+	{
+		return (_pow_(x, y - 1) * x);
+	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
+	else
+		return (1);
+}
+
+/**
+* binary_tree_size - pre-order traversal of a binary tree
+* @tree: pointer to the root node
+* Return: size_t
+*/
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+
+if (!tree)
+return (0);
+else
+return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+
+}
+
+
+/**
+* binary_tree_height - height of a binary tree
+* @tree: pointer to the root node
+* Return: int
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+size_t l, r;
+
+if (!tree || (!(tree->left) && !(tree->right)))
+return (0);
+
+l = binary_tree_height(tree->left);
+r = binary_tree_height(tree->right);
+if (l > r)
+return (l + 1);
+else
+return (r + 1);
+}
 
 /**
 * binary_tree_is_perfect- checks if a binary tree is full
@@ -16,7 +65,7 @@ return (0);
 height = binary_tree_height(tree);
 size = binary_tree_size(tree);
 
-h = pow(2.0, height + 1) - 1;
+h = _pow_(2.0, height + 1) - 1;
 if (h == size)
 return (1);
 else
