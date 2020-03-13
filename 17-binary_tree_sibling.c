fae4d9312f81b629181b108cfb2f678febb97257
inoublii <inoublii@gmail.com>
2020-03-13 16:40:30 +0100

    Fixing Betty

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 
 /**
-* binary_tree_is_full- checks if a binary tree is full
-* @tree: pointer to the root node
+* binary_tree_sibling- checks if a binary tree is full
+* @node: pointer to the root node
 * Return: int
 */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
@@ -13,11 +13,11 @@ return (NULL);
 
 if (node->parent->left == node)
 {
-  return (node->parent->right);
+return (node->parent->right);
 }
 if (node->parent->right == node)
 {
-  return (node->parent->left);
+return (node->parent->left);
 }
 return (NULL);
 }
